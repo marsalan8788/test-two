9920acdb6054394486cf5250424b35545e7bd606
marsalan8788 <86024607+marsalan8788@users.noreply.github.com>
2023-12-19 15:56:52 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1" # Update this to your desired region
+}
+
+# Create an EC2 instance
+resource "aws_instance" "simple_ec2" {
+  ami           = "ami-07207a5954220f067" # Update this to the desired AMI
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.simple_sg.id]
+
+  tags = {
+    Name = "Simple EC2 Instance"
+  }
+}
